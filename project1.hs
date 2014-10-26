2c1d0dd6e50b15d75f7727b3476a457df2796847
t00n <antoine.carpentier.info@gmail.com>
2014-10-26 18:54:53 +0100

    [add] lost function

--- project1.hs
+++ project1.hs
@@ -1,5 +1,9 @@
-
 module Project (Board(initialize, click, flag, won, lost), top) where
+
+----added
+--import Data.Sequence
+----
+
 
 -- The show instance must be highly customized to display a board in ASCII
 class Show b => Board b where
@@ -48,26 +52,36 @@ flag_loop (Just coord) board = do putStrLn "Place a flag???"
 
 ----------------------------------- My Code -----------------------------------
 
-data Cell = Flagged | Clicked | Masked
-data MyBoard = MyBoard [[Cell]]
+-- a cell is
+-- flagged, mine or not
+-- clicked, mine (-1) or number of adjacent mine
+-- masked, mine or not
+data Cell = Flagged Bool | Clicked Int | Masked Bool
+data MyBoard = MyBoard { val :: [[Cell]] }
 
 instance Show Cell where
-  show Flagged = "F"
-  show Clicked = "5"
-  show Masked = " "
+  show (Flagged _) = "F"
+  show (Clicked x)
+    | x == -1 = "M"
+    | otherwise = show x
+  show (Masked _) = " "
 
 instance Show MyBoard where
-  show (MyBoard []) = "\n"
-  show (MyBoard (xs:xss)) = (concat $ replicate ((length xs)) "+-") ++ "\n" -- boundaries
-                            ++ (concat $ ["|" ++ (show x) | x <- xs]) ++ "\n" -- values
+  show (MyBoard []) = ""
+  show (MyBoard (xs:xss)) = (concat $ replicate (length xs) "+-") ++ "\n" -- boundaries
+                            ++ (concat $ ["|" ++ (show x) | x <- xs]) ++ "\n" -- values for this line
                             ++ (show (MyBoard xss)) -- rest of the board
 
 instance Board MyBoard where
-  initialize seed (x,y) (c1,c2) = MyBoard $ replicate x (replicate y Masked)
+  initialize seed (x,y) (c1,c2) = MyBoard $ replicate x (replicate y (Masked False))
+    where nbOfMines = x*y `div` 10
   click (c1,c2) b = b
   flag (f1,f2) b = b
   won b = False
-  lost b = False
+  lost b = foldr clickedMine False (concat (val b))
+    where clickedMine (Flagged _) acc = acc || False
+          clickedMine (Masked _) acc = acc || False
+          clickedMine (Clicked a) acc  = acc || (a == -1)
 
 main :: IO()
 main = top (initialize :: Int -> (Int,Int) -> (Int,Int) -> MyBoard)
