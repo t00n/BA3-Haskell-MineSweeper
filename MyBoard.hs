9424953f9d510750ac1ccafd3fffa9f8817224ec
t00n <antoine.carpentier.info@gmail.com>
2014-11-03 11:31:28 +0100

    [ref] Cleaning Code

--- MyBoard.hs
+++ MyBoard.hs
@@ -1,7 +1,7 @@
 module MyBoard where
 
 import Data.Foldable as Fold (foldr)
-import Data.Vector as Vec hiding ((++), concat, update)
+import Data.Vector as Vec hiding ((++), concat, update, replicate, take, elem, map)
 import System.Random
 
 import Board
@@ -26,41 +26,47 @@ instance Show Cell where
   show (Masked False) = " "
   show (Masked True) = "M"
 
+-- display a beautiful board
 instance Show MyBoard where
   show b 
     | height b == 0 = ""
-    | otherwise = (concat $ Prelude.replicate w "+-") ++ "\n" -- boundaries
-                  ++ (concat $ Vec.toList $ Vec.map ("|" ++) $ Vec.map show xs) ++ "\n" -- values for this line
+    | otherwise = (concat $ replicate w "+-") ++ "\n" -- boundaries
+                  ++ (concat $ map ("|" ++) $ map show (toList xs)) ++ "\n" -- values for this line
                   ++ (show (MyBoard xss w newH)) -- rest of the board
                   where (xs, xss) = Vec.splitAt w (val b)
                         w = width b
                         newH = (height b)-1
 
+-- remove duplicates and one specified exception in a iterate-generated list (one at a time)
+removeDuplicates :: Eq a => a -> [(a, b)] -> [(a, b)]
+removeDuplicates exception aList = [x|(Just x, _, _) <- iterate getNext (Nothing, aList, [])]
+  where getNext (_, x:xs, used) 
+          | fst x `elem` used || fst x == exception = (Nothing, xs, used)
+          | otherwise = (Just x, xs, (fst x):used)
+
+-- generate a list of random Ints of size "n" in range "range" excluding duplicates and "exception"
+uniqueRandomInts :: (Random a, Eq a, RandomGen b) => a -> (a, a) -> Int -> b -> [a]
+uniqueRandomInts exception range n = map fst . take n . removeDuplicates exception . iterate getNext . randomR range
+  where getNext = randomR range . snd
+
 instance Board MyBoard Cell where
-  initialize seed (x,y) (c1,c2) = click (c1, c2) (MyBoard s x y)
-    where nbOfMines = x*y `div` 4
-          firstClick = c1*x + c2
-          size = x*y-1
-          range = (0, size)
-          removeDuplicates aList = [x|(Just x, _, _) <- iterate getNext (Nothing, aList, [])]
-            where getNext (_, x:xs, used) 
-                    | fst x `Prelude.elem` used || fst x == firstClick = (Nothing, xs, used)
-                    | otherwise = (Just x, xs, (fst x):used)
-          uniqueRandomInts = Prelude.map fst . Prelude.take nbOfMines . removeDuplicates . iterate getNext . randomR range
-            where getNext = randomR range . snd
-          gen = uniqueRandomInts (mkStdGen seed)
-          s = generate size (\i -> if i `Prelude.elem` gen then (Masked True) else (Masked False))
+  initialize seed (width,height) (c1,c2) = click (c1, c2) (MyBoard vec width height)
+    where nbOfMines = width*height `div` 4 -- 4 is a magic number !
+          firstClick = c1*width + c2
+          sizeVec = width*height-1
+          randomList = uniqueRandomInts firstClick (0, sizeVec) nbOfMines (mkStdGen seed)
+          vec = generate sizeVec (\i -> if i `elem` randomList then (Masked True) else (Masked False))
   get (x, y) b = (val b) ! (x*w + y)
     where w = width b
   update (x, y) a b 
-    | x < 0 || y < 0 || x >= (width b) || y >= (height b) = b
+    | x < 0 || y < 0 || x >= w || y >= h = b
     | otherwise = MyBoard ((val b) // [(i, a)]) w h
       where w = width b
             h = height b
             i = x*w + y
-  click (x, y) b 
+  click (x, y) b
     | nbOfAdjacentMines /= 0 = update (x, y) (newValue oldValue) b
-    | otherwise = Fold.foldr (\x acc -> click x acc) b neighbours
+    | otherwise = Fold.foldr (\x acc -> click x acc) (update (x, y) (newValue oldValue) b) neighbours
       where oldValue = get (x, y) b
             newValue (Masked True) = (Clicked (-1))
             newValue (Masked False) = (Clicked nbOfAdjacentMines)
