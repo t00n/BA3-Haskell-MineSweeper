47c1e3d3fa789433f59d95694b214a47cbcb9fd3
t00n <antoine.carpentier.info@gmail.com>
2015-01-08 17:30:38 +0100

    [fix] removed old stuff

--- MainGtk.hs
+++ MainGtk.hs
@@ -3,25 +3,68 @@
 module Main (main) where
 
 import Graphics.UI.Gtk as GTK
-import Graphics.UI.Gtk.ModelView as Model
-import Control.Monad.Trans.State as StateT
+import Data.IORef
 import Control.Monad.Trans
-import Control.Monad.State.Lazy as State
 
-import GUIState
 import Board
 import MyBoard
+
+data Options = Options {
+	seed :: Int,
+	size :: (Int, Int),
+	firstClick :: (Int, Int)
+}
+
+data ProgramState = ProgramState {
+	mainWindow :: Window,
+	optionsWindow :: Window,
+	board :: MyBoard,
+	buttons :: [[Button]],
+	options :: Options
+}
+
+dummyProgramState :: IO ProgramState
+dummyProgramState = do
+	mainW <- windowNew
+	optionsW <- windowNew
+	let b = initialize 0 (0,0) (0,0)
+	let opt = Options 0 (0,0) (0,0)
+	return $ ProgramState mainW optionsW b [[]] opt
 
 main :: IO ()
 main = do
 	initGUI
-	initWindowNew
+	ps <- dummyProgramState
+	ref <- newIORef ps
+	buildOptionsWindow ref
+	buildMainWindow ref
+	showOptionsWindow ref
 	mainGUI
 
-initWindowNew :: IO ()
-initWindowNew = do
+showMainWindow :: IORef ProgramState -> IO ()
+showMainWindow ref = do
+	ps <- readIORef ref
+	widgetShowAll (mainWindow ps)
+	widgetHide (optionsWindow ps)
+
+showOptionsWindow :: IORef ProgramState -> IO ()
+showOptionsWindow ref = do
+	ps <- readIORef ref
+	widgetShowAll (optionsWindow ps)
+	widgetHide (mainWindow ps)
+
+buildMainWindow :: IORef ProgramState -> IO Window
+buildMainWindow ref = do
+	ps <- readIORef ref
+	let w = mainWindow ps
+	onDestroy w mainQuit
+	return w
+
+buildOptionsWindow :: IORef ProgramState -> IO Window
+buildOptionsWindow ref = do
 	-- init
-	w <- windowNew
+	ps <- readIORef ref
+	let w = optionsWindow ps
 	vbox <- vBoxNew False 0
 	labelSeed <- labelNew $ Just "Seed"
 	entrySeed <- entryNew
@@ -35,10 +78,12 @@ initWindowNew = do
 		seed <- entryGetText entrySeed :: IO [Char]
 		size <- entryGetText entrySize :: IO [Char]
 		click <- entryGetText entryClick :: IO [Char]
-		guiState <- newGUIState $ initialize (read seed) (read size) (read click)
-		runStateT runGUI guiState
-		widgetHide w
-		return ()
+		let opt = Options (read seed) (read size) (read click)
+		let b = initialize (read seed) (read size) (read click)
+		let newPS = ProgramState (mainWindow ps) (optionsWindow ps) b (buttons ps) opt
+		writeIORef ref newPS
+		buildTable ref
+		showMainWindow ref
 	-- put objects in container
 	containerAdd vbox labelSeed
 	containerAdd vbox entrySeed
@@ -50,89 +95,88 @@ initWindowNew = do
 	-- window
 	GTK.set w [containerChild := vbox ]
 	onDestroy w mainQuit
-	widgetShowAll w
-	return ()
+	return w
 
-runGUI :: StateT GUIState IO ()
-runGUI = do
-	refreshTable
-	guiState <- StateT.get
-	liftIO $ onDestroy (window guiState) mainQuit
-	return ()
+buildTable :: IORef ProgramState -> IO ()
+buildTable ref = do
+	ps <- readIORef ref
+	let b = board ps
+	table <- tableNew (width b) (height b) True
+	let w = mainWindow ps
+	GTK.set w [ containerChild := table ]
+	buttonTable <- cellsToTable 0 (val b) table ref
+	writeIORef ref $ ProgramState w (optionsWindow ps) b buttonTable (options ps)
+	updateTable ref
 
-refreshTable :: StateT GUIState IO ()
-refreshTable = do
-	guiState <- StateT.get
-	let t = table guiState
-	let w = window guiState
-	newTable <- myBoardToTable
-	StateT.modify $ setTable newTable
-	liftIO $ widgetDestroy t
-	liftIO $ GTK.set w [ containerBorderWidth := 10, containerChild := newTable ]
-	liftIO $ widgetShowAll w
+cellsToTable :: Int -> [[Cell]] -> Table -> IORef ProgramState -> IO [[Button]]
+cellsToTable _ [] _ _ = return []
+cellsToTable i (xs:xss) table ref = do
+	buttonTable <- cellsToTable (i+1) xss table ref
+	buttonList <- cellsToRow (i, 0) xs table ref
+	let newButtonTable = buttonList : buttonTable
+	return newButtonTable
 
-
-myBoardToTable :: StateT GUIState IO Table
-myBoardToTable = do
-	guiState <- StateT.get
-	let b = board guiState
-	table <- liftIO $ tableNew (width b) (height b) True
-	cellsToTable 0 (val b) table
-
-
-cellsToTable :: Int -> [[Cell]] -> Table -> StateT GUIState IO Table
-cellsToTable _ [] table = return table
-cellsToTable i (xs:xss) table = do
-	newTable <- cellsToTable (i+1) xss table
-	cellsToRow (i, 0) xs newTable
-
-cellsToRow :: (Int, Int) -> [Cell] -> Table -> StateT GUIState IO Table
-cellsToRow _ [] table = return table
-cellsToRow (i, j) (x:xs) table = do
-	tableOutIO <- cellsToRow (i, (j+1)) xs table
-	StateT.modify $ setTable tableOutIO
-	guiState <- State.get
-	button <- liftIO $ cellToButton x
-	liftIO $ button `on` buttonPressEvent $ tryEvent $ do
+cellsToRow :: (Int, Int) -> [Cell] -> Table -> IORef ProgramState -> IO [Button]
+cellsToRow _ [] _ _ = return []
+cellsToRow (i, j) (x:xs) table ref = do
+	buttonList <- cellsToRow (i, (j+1)) xs table ref
+	button <- buttonNew
+	button `on` buttonPressEvent $ tryEvent $ do
 		LeftButton <- eventButton
-		liftIO $ onClickedCell (click (i, j) (board guiState)) guiState
-	liftIO $ button `on` buttonPressEvent $ tryEvent $ do
+		ps <- liftIO $ readIORef ref
+		let newBoard = click (i, j) (board ps)
+		liftIO $ writeIORef ref $ ProgramState (mainWindow ps) (optionsWindow ps) newBoard (buttons ps) (options ps)
+		liftIO $ updateTable ref
+	button `on` buttonPressEvent $ tryEvent $ do
 		RightButton <- eventButton
-		liftIO $ onClickedCell (flag (i, j) (board guiState)) guiState
-	liftIO $ tableAttachDefaults tableOutIO button i (i+1) j (j+1)
-	return tableOutIO
+		ps <- liftIO $ readIORef ref
+		let newBoard = flag (i, j) (board ps)
+		liftIO $ writeIORef ref $ ProgramState (mainWindow ps) (optionsWindow ps) newBoard (buttons ps) (options ps)
+		liftIO $ updateTable ref
+	let newButtonList = button : buttonList
+	tableAttachDefaults table button i (i+1) j (j+1)
+	return newButtonList
+
+updateTable :: IORef ProgramState -> IO ()
+updateTable ref = do
+	ps <- readIORef ref
+	let buttonTable = buttons ps
+	let b = val $ board ps
+	updateRow 0 b buttonTable
+	putStrLn $ show $ board ps
+
+updateRow :: Int -> [[Cell]] -> [[Button]] -> IO ()
+updateRow _ [] _ = return ()
+updateRow i (xs:xss) buttonTable = do
+	updateRow (i+1) xss buttonTable
+	updateCell (i, 0) xs buttonTable
+
+updateCell :: (Int, Int) -> [Cell] -> [[Button]] -> IO ()
+updateCell _ [] _ = return ()
+updateCell (i,j) (x:xs) buttonTable = do
+	updateCell (i, (j+1)) xs buttonTable
+	cellToButton x ((buttonTable!!i)!!j)
+
 
 -- create a gtk button from a cell
-cellToButton :: Cell -> IO Button
-cellToButton (Masked _) = do
-	button <- buttonNew
+cellToButton :: Cell -> Button -> IO ()
+cellToButton (Masked _) button = do
+	emptyButton button
 	image <- imageNewFromFile "masked.png"
-	containerAdd button image
-	return button
-cellToButton (Flagged _) = do
-	button <- buttonNew
+	buttonSetImage button image
+cellToButton (Flagged _) button = do
+	emptyButton button
 	image <- imageNewFromFile "flag.png"
-	containerAdd button image
-	return button
-cellToButton (Clicked (-1)) = do
-	button <- buttonNew
+	buttonSetImage button image
+cellToButton (Clicked (-1)) button = do
+	emptyButton button
 	image <- imageNewFromFile "mine.png"
-	containerAdd button image
-	return button
-cellToButton (Clicked x) = buttonNewWithLabel (show x)
+	buttonSetImage button image
+cellToButton (Clicked x) button = do
+	emptyButton button
+	buttonSetLabel button (show x)
 
--- cell clicked event
--- parameter 1 : the new board if this cell is clicked/flagged
--- parameter 2 : because it is in the IO monad, it must get the state as a parameter to modify it
-onClickedCell :: MyBoard -> GUIState -> IO ()
-onClickedCell b guiState = do
-	(x, y) <- runStateT (updateTable b) guiState
-	return x
-
--- called from onClickedCell to return in StateT monad and modify the state
-updateTable :: MyBoard -> StateT GUIState IO ()
-updateTable b = do
-	StateT.modify $ setBoard b
-	refreshTable
-	guiState <- StateT.get
-	fmap (\b -> ()) StateT.get
+emptyButton :: Button -> IO ()
+emptyButton button = do
+	children <- containerGetChildren button
+	containerForeach button (containerRemove button)
